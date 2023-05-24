776fd8c934bcd3204f4aa68b5413cd2521569668
leduyphong123 <leduyphongck789@gmail.com>
2023-05-24 15:53:22 +0700

    finnish

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import static entity.BinarySearch.binarySearch;
+import static entity.BinarySearch.list;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));  /* 0 */
+        System.out.println(binarySearch(list, 11)); /* 4 */
+        System.out.println(binarySearch(list, 79)); /*12 */
+        System.out.println(binarySearch(list, 1));  /*-1 */
+        System.out.println(binarySearch(list, 5));  /*-1 */
+        System.out.println(binarySearch(list, 80)); /*-1 */
+    }
+}
